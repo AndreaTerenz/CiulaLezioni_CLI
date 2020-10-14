909feed998a3da4db0d6973ab2bc58d81584ecc8
AndreaTerenziani <andrea.terenziani.at@gmail.com>
2020-10-14 19:03:50 +0200

    misc refactoring

--- ciulalezioni_cli.py
+++ ciulalezioni_cli.py
@@ -37,6 +37,30 @@ def get_cookies_file():
     
     return output
 
+def get_args_from_usr():
+    return get_input_url(), get_cookies_file(), input("Insert path to output directory (ENTER to use current directory) >>")
+
+def download(url:str, cookies:str, out_dir:str):
+    if not(out_dir.endswith(os.path.sep)) and (len(out_dir) > 0):
+        out_dir += os.path.sep
+
+    output = out_dir + "%(title)s.%(ext)s"
+
+    ydl_opts = {
+        'cookiefile': cookies,
+        'outtmpl' : output
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+def read_arg_from_cli(arg_pos:int, check_fun:function, err_msg:str):
+    if check_fun(sys.argv[arg_pos]):
+        return sys.argv[arg_pos]
+    else:
+        print(err_msg)
+        sys.exit(-1)
+
 if __name__ == "__main__":
     argc = len(sys.argv)-1
 
@@ -48,36 +72,13 @@ if __name__ == "__main__":
         out_dir = ""
 
         if (argc == 0):
-            input_url = get_input_url()
-            cookies_path = get_cookies_file()
-            out_dir = input("Insert path to output directory (ENTER to use current directory) >>")
+            input_url, cookies_path, out_dir = get_args_from_usr()
         else:
-            if check_url(sys.argv[1]):
-                input_url = sys.argv[1]
-            else:
-                print("Invalid input url")
-                sys.exit(-1)
-
-            if check_cookies_file(sys.argv[2]):
-                cookies_path = sys.argv[2]
-            else:
-                print("Cookies file is non existent or not a .txt file")
-                sys.exit(-1)
+            input_url = read_arg_from_cli(1, check_url, "Invalid input url")
+            cookies_path = read_arg_from_cli(2, check_cookies_file, "Cookies file is non existent or not a .txt file")
 
             if argc == 3:
                 out_dir = sys.argv[3]
 
-        if not(out_dir.endswith(os.path.sep)) and (len(out_dir) > 0):
-            out_dir += os.path.sep
-
-        output = out_dir + "%(title)s.%(ext)s"
-
-        ydl_opts = {
-            'cookiefile': cookies_path,
-            'outtmpl' : output
-        }
-
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([input_url])
-
+        download(input_url, cookies_path, out_dir)
 
