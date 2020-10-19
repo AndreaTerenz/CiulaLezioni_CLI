444cb12c1369b710ebb43961a2462823d81ce01d
AndreaTerenziani <andrea.terenziani.at@gmail.com>
2020-10-19 23:32:52 +0200

    Video urls can now be read from a text file

--- ciulalezioni_cli.py
+++ ciulalezioni_cli.py
@@ -3,6 +3,11 @@ import youtube_dl
 import argparse
 from urllib.parse import urlparse
 
+COOKIES_ARG = "cookies"
+URLS_LIST_ARG = "urls"
+URLS_FILE_ARG = "urls_file"
+OUT_DIR_ARG = "out_dir"
+
 def check_url(url:str, log=False):
     if (url.strip() != ""):
         res = urlparse(url)
@@ -58,10 +63,53 @@ def get_cookies_file():
 def get_args_from_usr():
     return get_input_urls(), get_cookies_file(), input("Insert path to output directory (ENTER to use current directory) >>")
 
-def download(url:str, cookies:str, out_dir:str):
+def parse_args_from_cli():
+    parser = argparse.ArgumentParser(description='wrapper for youtube dl to easily "steal" lectures from my teachers\' GDrive',
+                                     epilog='note that without any arguments the program will ask for them via stdin')
+
+    main_grp = parser.add_argument_group()
+
+    main_grp.add_argument("-o", "--"+OUT_DIR_ARG, nargs=1, default="", metavar="DIR",
+                        type=str, help="directory to download the videos in")
+    main_grp.add_argument("-c", "--"+COOKIES_ARG, required=True, nargs=1, type=str, help="Path to cookies txt file")
+
+    group = main_grp.add_mutually_exclusive_group(required=True)
+    group.add_argument("-u", "--"+URLS_LIST_ARG, type=str, nargs="+", help="Url of each video to be downloaded")
+    group.add_argument("-f", "--"+URLS_FILE_ARG, type=str, nargs=1, help="Path to txt file containing the urls of the video to download")
+
+    args = vars(parser.parse_args())
+
+    urls = None
+
+    if (args[URLS_LIST_ARG] != None):    
+        urls = list(args[URLS_LIST_ARG])
+    elif (args[URLS_FILE_ARG] != None):
+        try:
+            with open(args[URLS_FILE_ARG][0], mode="r") as f:
+                urls = f.readlines()
+        except EnvironmentError:
+            print("Failed to read urls from file")
+            sys.exit(-1)
+            
+    return check_input_args(urls, args[COOKIES_ARG][0], args[OUT_DIR_ARG][0])
+
+def check_input_args(urls:list, cookies:str, out_dir:str):
+    if not(check_cookies_file(cookies)):
+        print("Cookies file is non existent or not a .txt file")
+        sys.exit(-1)
+
+    urls = list(filter(lambda u: check_url(u, log=True), urls))
+
+    if len(urls) == 0:
+        print("No valid urls were provided")
+        sys.exit(-1)
+
     if not(out_dir.endswith(os.path.sep)) and (len(out_dir) > 0):
         out_dir += os.path.sep
 
+    return urls, cookies, out_dir
+
+def download(url:str, cookies:str, out_dir:str):
     output = out_dir + "%(title)s.%(ext)s"
 
     ydl_opts = {
@@ -72,49 +120,13 @@ def download(url:str, cookies:str, out_dir:str):
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-def read_arg_from_cli(arg_pos:int, check_fun, err_msg:str):
-    if check_fun(sys.argv[arg_pos]):
-        return sys.argv[arg_pos]
-    else:
-        print(err_msg)
-        sys.exit(-1)
-
 if __name__ == "__main__":
     input_urls = []
     cookies_path = ""
     out_dir = ""
 
-    if (len(sys.argv) - 1 == 0):
-        input_urls, cookies_path, out_dir = get_args_from_usr()
-    else:
-        parser = argparse.ArgumentParser(description='wrapper for youtube dl to easily "steal" lectures from my teachers\' GDrive',
-                                         epilog='note that without any arguments the program will ask for them via stdin')
+    input_urls, cookies_path, out_dir = parse_args_from_cli() if (len(sys.argv)-1 != 0) else get_args_from_usr()
 
-        COOKIES_ARG = "cookies"
-        URLS_ARG = "urls"
-        OUT_DIR_ARG = "out_dir"
-
-        parser.add_argument(COOKIES_ARG, nargs=1, type=str, help="Path to cookies txt file")
-        parser.add_argument(URLS_ARG, type=str, nargs="+", help="Url of each video to be downloaded")
-
-        parser.add_argument("-o", "--"+OUT_DIR_ARG, nargs=1, default="", metavar="DIR",
-                            type=str, help="directory to download the videos in")
-
-        args = parser.parse_args()
-
-        if not(check_cookies_file(vars(args)[COOKIES_ARG][0])):
-            print("Cookies file is non existent or not a .txt file")
-            sys.exit(-1)
-        
-        cookies_path = vars(args)[COOKIES_ARG][0]
-        out_dir = vars(args)[OUT_DIR_ARG][0]
-        
-        input_urls = list(filter(lambda u: check_url(u, log=True), vars(args)[URLS_ARG]))
-
-        if len(input_urls) == 0:
-            print("No valid urls were provided")
-            sys.exit(-1)
-        
     for i in range(0, len(input_urls)):
         u = input_urls[i]
 
